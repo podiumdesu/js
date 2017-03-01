991045d4ae8923477bf90f272ccba3a6bca3725f
podiumdesu <wwhmomo@gmail.com>
2017-03-01 23:59:10 +0800

    modified

--- jsdom_practice/displayAbbreviations/scripts/displayCitations.js
+++ jsdom_practice/displayAbbreviations/scripts/displayCitations.js
@@ -1,6 +1,6 @@
 function displayCitations() {
   var quotes = document.getElementsByTagName("blockquote");
-  if (quotes.length < 1) return false;
+  //if (quotes.length < 1) return false;
   for (var i = 0; i < quotes.length; i++) {
 //    var currentCite = cites[i];
     //var cite_text = currentCite.getAttribute("cite");
@@ -8,19 +8,22 @@ function displayCitations() {
     //display[i] = cite_text;
     if (!quotes[i].getAttribute("cite")) continue; //跳出此次循环
     var url = quotes[i].getAttribute("cite");
-    var quoteChildren = quotes.getElementsByTagName("*");
-    if (quoteChildren.length < 1) continue;
-    var elem = quoteChildren[quoteChildren.length - 1];
+    var quoteChildren = quotes[i].getElementsByTagName("*");  //获取所有的节点
+    //漏了一个i，啊为什么我一直犯错误啊
+    if (quoteChildren.length < 1) continue;　　//判断节点长度
+    var elem = quoteChildren[quoteChildren.length - 1];　　//找到最后一个节点
 
 
-    var link = document.createElement("a");
+    var links = document.createElement("a");
     var link_text = document.createTextNode("source");
-    link.appendChild(link_text);
 
-    link.setAttribute("href",url);
+    links.appendChild(link_text);
+
+    links.setAttribute("href",url);
 
     var insert = document.createElement("sup");
-    insert.appendChild(link);
+
+    insert.appendChild(links);
 
     elem.appendChild(insert);
 
